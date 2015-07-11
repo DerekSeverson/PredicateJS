298f8755835e9acf6bceccc02ef9d4e529c2502a
DerekSeverson <derek@swarmingtech.com>
2015-07-11 00:09:28 -0500

    makes the Predicate module available in mocha tests

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,7 +1,21 @@
 
+// Testing Helpers
+var chai = require('chai');
+var assert = chai.assert;
+var expect = chai.expect;
 
-describe('Mocha Tests', function () {
-  it('Hello, World', function () {
-    
+// Require the Module we're testing & lodash for usefulness
+var _ = require('lodash');
+var Predicate = require('../index.js');
+
+describe('Predicate Module', function () {
+  
+  it ('can be required via CommonJS', function () {
+    expect(Predicate).to.exist;
   });
+  
+  it ('is a function', function () {
+    assert(_.isFunction(Predicate), 'Predicate is a function');
+  });
+  
 });
